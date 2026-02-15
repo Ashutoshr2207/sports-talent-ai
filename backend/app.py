568060c83a7ffcad37868242061662d760aeb6b5
Ashutosh <ashutoshramani38@gmail.com>
2026-02-15 15:51:43 +0530

    Initial backend working

---
+++ backend/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return jsonify({"message": "Backend working"})
+
+app.run(debug=True)
